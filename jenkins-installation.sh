30b9f05f54862c186f17d3aa003014db7a0184a8
Sibiya-G <sibiya22.06.2005@gmail.com>
2025-07-18 11:42:18 +0530

    Create jenkins-installation.sh

---
+++ jenkins-installation.sh
@@ -0,0 +1,43 @@
+pipeline {
+    agent label ''
+
+    environment {
+       AWS_CREDENTIALS_ID = credentials'394ca990-960f-42bd-b9e7-6af5a8dea834'  // Replace with actual AWS credential ID
+    }
+
+    parameters {
+        choice(name: 'ACTION', choices: ['apply', 'destroy'], description: 'Choose Terraform action')
+    }
+
+    stages {
+        stage('Checkout SCM') {
+            steps {
+                git branch: 'main', url: 'https://github.com/harris-97/jenkins-terraform-test.git'
+            }
+        }
+
+        stage('Initialize Terraform') {
+            steps {
+                sh 'terraform init'
+            }
+        }
+
+        stage('Plan Terraform') {
+            steps {
+                sh 'terraform plan -out=tfplan'
+            }
+        }
+
+        stage('Apply or Destroy') {
+            steps {
+                script {
+                    if (params.ACTION == 'apply') {
+                        sh 'terraform apply -auto-approve tfplan'
+                    } else {
+                        sh 'terraform destroy -auto-approve'
+                    }
+                }
+            }
+        }
+    }
+}
